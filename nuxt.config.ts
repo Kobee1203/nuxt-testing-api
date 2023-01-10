a835474ea2a34a18e742ed65f5a612f54d4591f9
Kobee1203 <Kobee1203@users.noreply.github.com>
2023-01-10 10:26:49 +0100

    1 file modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
-    head: {
-      noscript: [{ children: 'Javascript is required' }],
-    },
-  },
-  vite: {
-    server: {
-      hmr: {
-        host: 'localhost',
-      },
-    },
+    baseURL: '/hotel',
   },
 });
