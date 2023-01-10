9ee40da95f5ebdd401d99b7378fb1960969aaad3
Kobee1203 <Kobee1203@users.noreply.github.com>
2023-01-10 10:06:57 +0100

    1 file modified

--- tests/server/api/api.test.ts
+++ tests/server/api/api.test.ts
@@ -1,16 +1,16 @@
 import { describe, expect, it, beforeEach } from 'vitest';
 import { fetch, $fetch, isDev, setup, url } from '@nuxt/test-utils';
 
-describe('example', async () => {
+describe('Testing Server API', async () => {
   await setup({
     //server: true,
     dev: true,
     setupTimeout: 240000,
   });
 
-  it('Renders Hello Nuxt', async () => {
-    console.log('**URL: ' + url('/api/api'));
-    const data = await $fetch('/api/api');
+  it('Fecthing API', async () => {
+    console.log('**URL: ' + url('/hotel/api/api'));
+    const data = await $fetch('/hotel/api/api');
     console.log('data', data);
     expect(data).toMatch('OK');
   });
